d5585c4a1d095811b27b0ceb69788f91938c5ee1
manish <mjha205@rku.ac.in>
2024-06-10 21:54:07 +0530

    removing build files

--- build/utils/catchAsyncErrorhandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncFunction = void 0;
-const asyncFunction = (Func) => (req, res, next) => {
-    Promise.resolve(Func(req, res, next)).catch(next);
-};
-exports.asyncFunction = asyncFunction;
